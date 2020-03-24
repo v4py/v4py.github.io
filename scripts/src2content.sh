bacd99400eb7ece985b4f73dc2ab2d1f3e8776ea
David Lukes <dafydd.lukes@gmail.com>
2020-03-24 12:11:29 +0100

    Primitive caching of generated notebooks

--- scripts/src2content.sh
+++ scripts/src2content.sh
@@ -5,15 +5,20 @@ set -e
 script_dir=$( dirname $( realpath "$0" ) )
 root=$( realpath "$script_dir"/.. )
 
+mtime() {
+  stat -c %Y $( realpath "$1" )
+}
+
 cd "$root"
-rm -rf content
+mkdir -p content
 # need to specify full path to src for cp to be able to make symlinks
-cp -rs "$root"/src content
+cp -rfsT "$root"/src content
 
 cd content
-find -iname \*.md | while read fname; do
-  if [ "$( basename "$fname" )" != LICENSE.md ]; then
-    jupytext --to notebook --execute "$fname"
-    rm -f "$fname"
+find -iname \*.md -not -name LICENSE.md | while read in_path; do
+  out_path=$( echo "$in_path" | sed 's/.md$/.ipynb/' )
+  if [ ! -f "$out_path" ] || [ $( mtime "$in_path" ) -gt $( mtime "$out_path" ) ]; then
+    jupytext --execute "$in_path" --output "$out_path"
   fi
+  rm -f "$in_path"
 done
